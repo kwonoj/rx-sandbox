87e988f499b41e6fe9d701adf9b6a74582a71ea4
OJ Kwon <kwon.ohjoong@gmail.com>
2017-08-17 12:28:48 -0700

    feat(parseobservablemarble): support expanding timeframe

--- src/marbles/parseObservableMarble.ts
+++ src/marbles/parseObservableMarble.ts
@@ -19,7 +19,13 @@ const parseObservableMarble = <T>(
   const frameOffset = subscriptionIndex < 0 ? 0 : subscriptionIndex;
 
   const values = Array.from(marble).filter(token => token !== ObservableMarbleToken.NOOP).slice(frameOffset).reduce((
-    acc: { currentOffset: number; messages: Array<TestMessage<T>>; simultaneousGrouped: boolean },
+    acc: {
+      currentOffset: number;
+      messages: Array<TestMessage<T>>;
+      simultaneousGrouped: boolean;
+      expanding: boolean;
+      expandingValue: Array<string>;
+    },
     token: any
   ) => {
     let message: TestMessage<T> | null = null;
@@ -35,6 +41,14 @@ const parseObservableMarble = <T>(
         message = new TestMessageValue<T>(acc.currentOffset, Notification.createComplete());
         break;
       case ObservableMarbleToken.TIMEFRAME_EXPAND:
+        if (acc.expandingValue.length === 0 && !acc.expanding) {
+          acc.expandingValue.splice(0);
+          acc.expanding = true;
+        } else if (acc.expandingValue.length > 0 && acc.expanding) {
+          acc.expanding = false;
+          const expandedFrame = parseInt(acc.expandingValue.join(''), 10);
+          acc.currentOffset += expandedFrame * frameTimeFactor;
+        }
         break;
       case ObservableMarbleToken.SIMULTANEOUS_START:
         acc.simultaneousGrouped = true;
@@ -46,8 +60,12 @@ const parseObservableMarble = <T>(
       case SubscriptionMarbleToken.SUBSCRIBE:
         break;
       default:
-        const customValue = value && value[token] ? value[token] : token;
-        message = new TestMessageValue<T>(acc.currentOffset, Notification.createNext<T>(customValue));
+        if (acc.expanding) {
+          acc.expandingValue.push(token);
+        } else {
+          const customValue = value && value[token] ? value[token] : token;
+          message = new TestMessageValue<T>(acc.currentOffset, Notification.createNext<T>(customValue));
+        }
     }
 
     if (!!message) {
@@ -61,7 +79,9 @@ const parseObservableMarble = <T>(
   }, {
     currentOffset: frameOffset,
     messages: [],
-    simultaneousGrouped: false
+    simultaneousGrouped: false,
+    expanding: false,
+    expandingValue: []
   });
 
   return values.messages;
