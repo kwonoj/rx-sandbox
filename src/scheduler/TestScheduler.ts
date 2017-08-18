e4b37bf7be0f24dab63199691a005403bf897054
OJ Kwon <kwon.ohjoong@gmail.com>
2017-08-17 20:37:05 -0700

    feat(testscheduler): define public interfaces

--- src/scheduler/TestScheduler.ts
+++ src/scheduler/TestScheduler.ts
@@ -1,15 +1,34 @@
 import { Observable } from 'rxjs/Observable';
+import { VirtualAction } from 'rxjs/scheduler/VirtualTimeScheduler';
 import { VirtualTimeScheduler } from 'rxjs/scheduler/VirtualTimeScheduler';
 import { ColdObservable } from 'rxjs/testing/ColdObservable';
 import { HotObservable } from 'rxjs/testing/HotObservable';
+import { parseObservableMarble } from '../marbles/parseObservableMarble';
 import { SubscriptionMarbleToken } from '../marbles/SubscriptionMarbleToken';
 import { TestMessage } from '../message/TestMessage';
 
-const parseMarbles = (..._args: Array<any>) => {
-  return [];
-};
-
 class TestScheduler extends VirtualTimeScheduler {
+  private readonly coldObservables: Array<ColdObservable<any>> = [];
+  private readonly hotObservables: Array<HotObservable<any>> = [];
+
+  constructor(private readonly autoFlush = false, private readonly frameTimeFactor = 1) {
+    super(VirtualAction, Number.POSITIVE_INFINITY);
+  }
+
+  public getMarbles(): void {
+    if (this.autoFlush) {
+      throw new Error('not implemented');
+    }
+  }
+
+  public advanceBy(_frameTime: number): void {
+    throw new Error('not implemented');
+  }
+
+  public advanceTo(_frameTime: number): void {
+    throw new Error('not implemented');
+  }
+
   public createColdObservable<T = string>(marble: string, value?: { [key: string]: T }, error?: any): Observable<T>;
   public createColdObservable<T = string>(message: Array<TestMessage<T>>): Observable<T>;
   public createColdObservable<T = string>(...args: Array<any>): Observable<T> {
@@ -19,8 +38,11 @@ class TestScheduler extends VirtualTimeScheduler {
       throw new Error(`Cold observable cannot have subscription offset ${SubscriptionMarbleToken.SUBSCRIBE}`);
     }
 
-    const messages = Array.isArray(marbleValue) ? marbleValue : parseMarbles(marbleValue, value, error);
+    const messages = Array.isArray(marbleValue)
+      ? marbleValue
+      : parseObservableMarble(marbleValue, value, error, false, this.frameTimeFactor);
     const observable = new ColdObservable<T>(messages, this);
+    this.coldObservables.push(observable);
     return observable;
   }
 
@@ -29,8 +51,11 @@ class TestScheduler extends VirtualTimeScheduler {
   public createHotObservable<T = string>(...args: Array<any>): Observable<T> {
     const [marbleValue, value, error] = args;
 
-    const messages = Array.isArray(marbleValue) ? marbleValue : parseMarbles(marbleValue, value, error);
+    const messages = Array.isArray(marbleValue)
+      ? marbleValue
+      : parseObservableMarble(marbleValue, value, error, false, this.frameTimeFactor);
     const subject = new HotObservable<T>(messages, this);
+    this.hotObservables.push(subject);
     return subject;
   }
 }
