4e04e9f8df00a186253f39acddbdba2b3611bf99
OJ Kwon <kwon.ohjoong@gmail.com>
2017-08-18 13:04:50 -0700

    refactor(testscheduler): rename getMarbles into getMessages

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,67 @@ import { TestScheduler } from './scheduler/TestScheduler';
 
 //workaround TS4029 by explicitly import types and avoid unused import error
 (() => Observable.toString())();
-(() => SubscriptionLog.toString())();
 (() => ColdObservable.toString())();
 (() => HotObservable.toString())();
 
-const rxSandbox = {
+export type RxSandboxInstance = {
+  /**
+   * Creates a hot observable using marble diagram DSL, or TestMessage.
+   */
+  hot: typeof TestScheduler.prototype.createHotObservable;
+  /**
+   * Creates a cold obsrevable using marbld diagram DSL, or TestMessage.
+   */
+  cold: typeof TestScheduler.prototype.createColdObservable;
+  /**
+   * Flush out currently scheduled observables, fill values returned by `getMarbles`.
+   */
+  flush: typeof TestScheduler.prototype.flush;
+  /**
+   * Get array of observable value's metadata TestMessage<T> from observable
+   * created via `hot` or `cold`. Returned array will be filled once scheduler flushes
+   * scheduled actions, either via explicit `flush` or implicit `autoFlush`.
+   */
+  getMessages: typeof TestScheduler.prototype.getMessages;
+  /**
+   * Utility function to generate `expected` values via marble diagram.
+   */
+  e: <T = string>(
+    marble: string,
+    value?: { [key: string]: T } | null,
+    error?: any
+  ) => Readonly<Array<TestMessage<T | Array<TestMessage<T>>>>>;
+  /**
+   * Utility function to generate `expected` subscriptions via marble diagram.
+   */
+  s: (marble: string) => SubscriptionLog;
+};
+
+export interface RxSandbox {
+  /**
+   * Creates new instance of test scheduler for testing observables.
+   *
+   * @param {boolean} [autoFlush] Flush scheduler automatically when `getMarbles` is being called. False by default.
+   * @param {number} [frameTimeFactor] Custom frametime factor for virtual time frame. 1 by default.
+   *
+   * @return {RxSandboxInstance} instance of test scheduler interfaces.
+   */
+  create(autoFlush?: boolean, frameTimeFactor?: number): RxSandboxInstance;
+  /**
+   * Utility assertion method to assert marble based observable test messages.
+   * By default return values of sandbox functions are plain object works with
+   * any testing framework or assertion library, while this assertion provides
+   * better visualization against observable test messages.
+   *
+   */
+  marbleAssert(): {
+    to: {
+      equal: () => void;
+    };
+  };
+}
+
+const rxSandbox: RxSandbox = {
   create: (autoFlush: boolean = false, frameTimeFactor: number = 1) => {
     const scheduler = new TestScheduler(autoFlush, frameTimeFactor);
 
@@ -21,12 +77,19 @@ const rxSandbox = {
       hot: scheduler.createHotObservable.bind(scheduler) as typeof scheduler.createHotObservable,
       cold: scheduler.createColdObservable.bind(scheduler) as typeof scheduler.createColdObservable,
       flush: scheduler.flush.bind(scheduler) as typeof scheduler.flush,
-      getMarbles: scheduler.getMarbles.bind(scheduler) as typeof scheduler.getMarbles,
+      getMessages: scheduler.getMessages.bind(scheduler) as typeof scheduler.getMessages,
       e: <T = string>(marble: string, value?: { [key: string]: T } | null, error?: any) =>
         parseObservableMarble(marble, value, error, true, frameTimeFactor),
       s: (marble: string) => parseSubscriptionMarble(marble, frameTimeFactor)
     };
-  }
+  },
+  marbleAssert: () => ({
+    to: {
+      equal: () => {
+        throw new Error('not implemented');
+      }
+    }
+  })
 };
 
 export { rxSandbox, TestMessage };
