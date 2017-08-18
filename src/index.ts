c42845caa46b11a53271d522063a23bd20834b82
OJ Kwon <kwon.ohjoong@gmail.com>
2017-08-18 12:09:16 -0700

    feat(index): export utility functions

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import { Observable } from 'rxjs/Observable';
+import { SubscriptionLog } from 'rxjs/testing/SubscriptionLog';
+import { parseObservableMarble } from './marbles/parseObservableMarble';
+import { parseSubscriptionMarble } from './marbles/parseSubscriptionMarble';
 import { TestMessage } from './message/TestMessage';
 import { TestScheduler } from './scheduler/TestScheduler';
 
 //workaround TS4029 by explicitly import types and avoid unused import error
 (() => Observable.toString())();
+(() => SubscriptionLog.toString())();
 
 const rxSandbox = {
   create: (autoFlush: boolean = false, frameTimeFactor: number = 1) => {
@@ -15,7 +19,10 @@ const rxSandbox = {
       flush: scheduler.flush.bind(scheduler) as typeof scheduler.flush,
       advanceBy: scheduler.advanceBy.bind(scheduler) as typeof scheduler.advanceBy,
       advanceTo: scheduler.advanceTo.bind(scheduler) as typeof scheduler.advanceTo,
-      getMarbles: scheduler.getMarbles.bind(scheduler) as typeof scheduler.getMarbles
+      getMarbles: scheduler.getMarbles.bind(scheduler) as typeof scheduler.getMarbles,
+      e: <T = string>(marble: string, value?: { [key: string]: T } | null, error?: any) =>
+        parseObservableMarble(marble, value, error, true, frameTimeFactor),
+      s: (marble: string) => parseSubscriptionMarble(marble, frameTimeFactor)
     };
   }
 };
