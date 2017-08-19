41fd0065d788fab6324989ca61bcf34fc501da4f
OJ Kwon <kwon.ohjoong@gmail.com>
2017-08-18 18:49:53 -0700

    test(calculatesubscriptionframe): add test cases

---
+++ spec/scheduler/calculateSubscriptionFrame-spec.ts
@@ -0,0 +1,68 @@
+import { expect } from 'chai';
+import 'rxjs/add/observable/of';
+import { Observable } from 'rxjs/Observable';
+import { calculateSubscriptionFrame } from '../../src/scheduler/calculateSubscriptionFrame';
+import { TestScheduler } from '../../src/scheduler/TestScheduler';
+
+describe('calculateSubscriptionFrame', () => {
+  it('should return immediate subscription without subscription token', () => {
+    const { subscribedFrame, unsubscribedFrame } = calculateSubscriptionFrame(Observable.of(''), '---!');
+
+    expect(subscribedFrame).to.equal(0);
+    expect(unsubscribedFrame).to.equal(3);
+  });
+
+  it('should preserve subscription frame with hot observable', () => {
+    const scheduler = new TestScheduler();
+    const hot = scheduler.createHotObservable('');
+
+    const { subscribedFrame, unsubscribedFrame } = calculateSubscriptionFrame(hot, '--^---!');
+
+    expect(subscribedFrame).to.equal(2);
+    expect(unsubscribedFrame).to.equal(6);
+  });
+
+  it('should return adjusted subscription frame with cold observable', () => {
+    const scheduler = new TestScheduler();
+    const hot = scheduler.createColdObservable('');
+
+    const { subscribedFrame, unsubscribedFrame } = calculateSubscriptionFrame(hot, '--^---!');
+
+    expect(subscribedFrame).to.equal(0);
+    expect(unsubscribedFrame).to.equal(4);
+  });
+
+  it('should preserve immediate subscription with cold observable', () => {
+    const scheduler = new TestScheduler();
+    const hot = scheduler.createColdObservable('');
+
+    const { subscribedFrame, unsubscribedFrame } = calculateSubscriptionFrame(hot, '^---!');
+
+    expect(subscribedFrame).to.equal(0);
+    expect(unsubscribedFrame).to.equal(4);
+  });
+
+  it('should return subscription only frame with hot obsrevable', () => {
+    const scheduler = new TestScheduler();
+    const hot = scheduler.createHotObservable('');
+
+    const { subscribedFrame, unsubscribedFrame } = calculateSubscriptionFrame(hot, '--^---');
+
+    expect(subscribedFrame).to.equal(2);
+    expect(unsubscribedFrame).to.equal(Number.POSITIVE_INFINITY);
+  });
+
+  it('should return adjusted subscription only frame with cold obsrevable', () => {
+    const scheduler = new TestScheduler();
+    const hot = scheduler.createColdObservable('');
+
+    const { subscribedFrame, unsubscribedFrame } = calculateSubscriptionFrame(hot, '--^---');
+
+    expect(subscribedFrame).to.equal(0);
+    expect(unsubscribedFrame).to.equal(Number.POSITIVE_INFINITY);
+  });
+
+  it('should throw when source is neither hot nor cold', () => {
+    expect(() => calculateSubscriptionFrame(Observable.of(''), '-^-!')).to.throw();
+  });
+});
