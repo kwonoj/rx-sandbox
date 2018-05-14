d1668718abc27ac59f3d075ff7e9d98aace7d187
Eric Adamski <er.adamski@gmail.com>
2018-05-14 13:35:18 -0400

    Accidentally ran preitter on this file, revert prettier styling

--- src/scheduler/TestScheduler.ts
+++ src/scheduler/TestScheduler.ts
@@ -26,11 +26,7 @@ class TestScheduler extends VirtualTimeScheduler {
     return this._maxFrame;
   }
 
-  constructor(
-    private readonly autoFlush: boolean,
-    private readonly frameTimeFactor: number,
-    maxFrameValue: number
-  ) {
+  constructor(private readonly autoFlush: boolean, private readonly frameTimeFactor: number, maxFrameValue: number) {
     super(VirtualAction, Number.POSITIVE_INFINITY);
     this._maxFrame = maxFrameValue * frameTimeFactor;
   }
@@ -39,28 +35,16 @@ class TestScheduler extends VirtualTimeScheduler {
     this.flushUntil();
   }
 
-  public getMessages<T = string>(
-    observable: Observable<T>,
-    unsubscriptionMarbles: string | null = null
-  ) {
+  public getMessages<T = string>(observable: Observable<T>, unsubscriptionMarbles: string | null = null) {
     const { subscribedFrame, unsubscribedFrame } = calculateSubscriptionFrame(
       observable,
       unsubscriptionMarbles,
       this.frameTimeFactor
     );
 
-    const observableMetadata: Array<
-      TestMessage<T | Array<TestMessage<T>>>
-      > = [];
-    const pushMetadata = (
-      notification: Notification<T | Array<TestMessage<T>>>
-    ) =>
-      observableMetadata.push(
-        new TestMessageValue<T | Array<TestMessage<T>>>(
-          this.frame,
-          notification
-        )
-      );
+    const observableMetadata: Array<TestMessage<T | Array<TestMessage<T>>>> = [];
+    const pushMetadata = (notification: Notification<T | Array<TestMessage<T>>>) =>
+      observableMetadata.push(new TestMessageValue<T | Array<TestMessage<T>>>(this.frame, notification));
 
     let subscription: Subscription | null = null;
     this.schedule(() => {
@@ -68,9 +52,7 @@ class TestScheduler extends VirtualTimeScheduler {
         (value: T) =>
           pushMetadata(
             Notification.createNext(
-              value instanceof Observable
-                ? this.materializeInnerObservable<T>(value, this.frame)
-                : value
+              value instanceof Observable ? this.materializeInnerObservable<T>(value, this.frame) : value
             )
           ),
         (err: any) => pushMetadata(Notification.createError(err)),
@@ -84,9 +66,7 @@ class TestScheduler extends VirtualTimeScheduler {
 
     if (this.autoFlush) {
       if (this.flushed) {
-        throw new Error(
-          `Cannot schedule to get marbles, scheduler's already flushed`
-        );
+        throw new Error(`Cannot schedule to get marbles, scheduler's already flushed`);
       }
       this.flush();
     }
@@ -94,98 +74,53 @@ class TestScheduler extends VirtualTimeScheduler {
     return observableMetadata;
   }
 
-  public createColdObservable<T = string>(
-    marble: string,
-    value?: { [key: string]: T } | null,
-    error?: any
-  ): ColdObservable<T>;
-  public createColdObservable<T = string>(
-    message: Array<TestMessage<T>>
-  ): ColdObservable<T>;
-  public createColdObservable<T = string>(
-    ...args: Array<any>
-  ): ColdObservable<T> {
+  public createColdObservable<T = string>(marble: string, value?: { [key: string]: T } | null, error?: any): ColdObservable<T>;
+  public createColdObservable<T = string>(message: Array<TestMessage<T>>): ColdObservable<T>;
+  public createColdObservable<T = string>(...args: Array<any>): ColdObservable<T> {
     const [marbleValue, value, error] = args;
 
-    if (
-      typeof marbleValue === "string" &&
-      marbleValue.indexOf(SubscriptionMarbleToken.SUBSCRIBE) !== -1
-    ) {
-      throw new Error(
-        `Cold observable cannot have subscription offset ${
-        SubscriptionMarbleToken.SUBSCRIBE
-        }`
-      );
+    if (typeof marbleValue === "string" && marbleValue.indexOf(SubscriptionMarbleToken.SUBSCRIBE) !== -1) {
+      throw new Error(`Cold observable cannot have subscription offset ${SubscriptionMarbleToken.SUBSCRIBE}`);
     }
 
     const messages = Array.isArray(marbleValue)
       ? marbleValue
       : parseObservableMarble(marbleValue, value, error, false, this.frameTimeFactor, this._maxFrame) as any;
-    const observable = new ColdObservable<T>(
-      messages as Array<TestMessage<T | Array<TestMessage<T>>>>,
-      this
-    );
+    const observable = new ColdObservable<T>(messages as Array<TestMessage<T | Array<TestMessage<T>>>>, this);
     this.coldObservables.push(observable);
     return observable;
   }
 
-  public createHotObservable<T = string>(
-    marble: string,
-    value?: { [key: string]: T } | null,
-    error?: any
-  ): HotObservable<T>;
-  public createHotObservable<T = string>(
-    message: Array<TestMessage<T>>
-  ): HotObservable<T>;
-  public createHotObservable<T = string>(
-    ...args: Array<any>
-  ): HotObservable<T> {
+  public createHotObservable<T = string>(marble: string, value?: { [key: string]: T } | null, error?: any): HotObservable<T>;
+  public createHotObservable<T = string>(message: Array<TestMessage<T>>): HotObservable<T>;
+  public createHotObservable<T = string>(...args: Array<any>): HotObservable<T> {
     const [marbleValue, value, error] = args;
 
     const messages = Array.isArray(marbleValue)
       ? marbleValue
-      : (parseObservableMarble(
-        marbleValue,
-        value,
-        error,
-        false,
-        this.frameTimeFactor,
-        this._maxFrame
-      ) as any);
-    const subject = new HotObservable<T>(
-      messages as Array<TestMessage<T | Array<TestMessage<T>>>>,
-      this
-    );
+      : parseObservableMarble(marbleValue, value, error, false, this.frameTimeFactor, this._maxFrame) as any;
+    const subject = new HotObservable<T>(messages as Array<TestMessage<T | Array<TestMessage<T>>>>, this);
     this.hotObservables.push(subject);
     return subject;
   }
 
   public advanceTo(toFrame: number): void {
     if (this.autoFlush) {
-      throw new Error(
-        "Cannot advance frame manually with autoflushing scheduler"
-      );
+      throw new Error("Cannot advance frame manually with autoflushing scheduler");
     }
 
     if (toFrame < 0 || toFrame < this.frame) {
-      throw new Error(
-        `Cannot advance frame, given frame is either negative or smaller than current frame`
-      );
+      throw new Error(`Cannot advance frame, given frame is either negative or smaller than current frame`);
     }
 
     this.flushUntil(toFrame);
     this.frame = toFrame;
   }
 
-  private materializeInnerObservable<T>(
-    observable: Observable<any>,
-    outerFrame: number
-  ): Array<TestMessage<T>> {
+  private materializeInnerObservable<T>(observable: Observable<any>, outerFrame: number): Array<TestMessage<T>> {
     const innerObservableMetadata: Array<TestMessage<T>> = [];
     const pushMetaData = (notification: Notification<T>) =>
-      innerObservableMetadata.push(
-        new TestMessageValue<T>(this.frame - outerFrame, notification)
-      );
+      innerObservableMetadata.push(new TestMessageValue<T>(this.frame - outerFrame, notification));
 
     observable.subscribe(
       value => pushMetaData(Notification.createNext(value)),
