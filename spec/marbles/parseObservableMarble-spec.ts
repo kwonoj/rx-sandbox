565f0142b6d3007b2a7b1d8b5963063dc6360383
Eric Adamski <er.adamski@gmail.com>
2018-05-14 13:29:37 -0400

    PR review fixes

--- spec/marbles/parseObservableMarble-spec.ts
+++ spec/marbles/parseObservableMarble-spec.ts
@@ -30,12 +30,7 @@ describe('parseObservableMarble', () => {
   it('should correctly parse falsy timeframe value', () => {
     const marble = '--a-b-c-d';
 
-    const messages = parseObservableMarble(marble, {
-      a: null,
-      b: false,
-      c: 0,
-      d: undefined
-    });
+    const messages = parseObservableMarble(marble, { a: null, b: false, c: 0, d: undefined });
     const expected = [next(2, null), next(4, false), next(6, 0), next(8, 'd')];
 
     expect(messages).to.deep.equal(expected);
