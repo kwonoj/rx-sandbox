3f407573fc702432029eae865d1c92103e154b73
OJ Kwon <kwon.ohjoong@gmail.com>
2021-05-18 13:44:52 -0700

    feat(sandbox): support v7.0.1
    BREAKING CHANGES: requires rxjs@7.0.1 and above.

--- src/utils/coreInternalImport.ts
+++ src/utils/coreInternalImport.ts
@@ -2,23 +2,11 @@
  * RxSandbox uses couple of undocumented internal api surface in rxjs.
  * This module reexports internal and its types.
  *
- * Since rxjs core does not export these interfaces directly,
- * types are force augmented via ambient.ts
  */
-import './ambient';
 
-import { COMPLETE_NOTIFICATION, errorNotification, nextNotification } from 'rxjs/dist/cjs/internal/Notification';
-import { AsyncAction } from 'rxjs/dist/cjs/internal/scheduler/AsyncAction';
-import { ColdObservable } from 'rxjs/dist/cjs/internal/testing/ColdObservable';
-import { HotObservable } from 'rxjs/dist/cjs/internal/testing/HotObservable';
-import { SubscriptionLog } from 'rxjs/dist/cjs/internal/testing/SubscriptionLog';
+import { AsyncAction } from 'rxjs/internal/scheduler/AsyncAction';
+import { ColdObservable } from 'rxjs/internal/testing/ColdObservable';
+import { HotObservable } from 'rxjs/internal/testing/HotObservable';
+import { SubscriptionLog } from 'rxjs/internal/testing/SubscriptionLog';
 
-export {
-  SubscriptionLog,
-  COMPLETE_NOTIFICATION,
-  errorNotification,
-  nextNotification,
-  HotObservable,
-  ColdObservable,
-  AsyncAction,
-};
+export { SubscriptionLog, HotObservable, ColdObservable, AsyncAction };
